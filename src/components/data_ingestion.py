773fa40f2424aeab45a39b09a1f48a84320ee77e
Sushravya <sushravya.mohan@gmail.com>
2024-10-26 12:52:47 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,18 +1,23 @@
 from email import header
 import os
 import sys
+from src.components import data_transformation
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import (
+    DataTransformation,
+    DataTransformationConfig,
+)
 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join("artifact", "train.csv")
-    test_data_path: str = os.path.join("artifact", "test.csv")
-    raw_data_path: str = os.path.join("artifact", "raw.csv")
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "raw.csv")
 
 
 class DataIngestion:
@@ -44,10 +49,13 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path,
             )
-        except:
+        except Exception as e:
             raise CustomException(e, sys)
 
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
